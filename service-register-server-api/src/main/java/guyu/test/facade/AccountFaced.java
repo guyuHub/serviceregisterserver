1af76788a17e1f02e8448d5e18133ead4239ea21
guyu <1712473752@qq.com>
2019-04-04 10:13:16 +0800

    完善

--- service-register-server-api/src/main/java/guyu/test/facade/AccountFaced.java
+++
@@ -1,8 +0,0 @@
-package guyu.test.facade;
-
-import guyu.test.model.AccountDTO;
-
-public interface AccountFaced {
-
-    public AccountDTO queryAccountByAccountId(Long service-register-serverId);
-}
---
+++ service-register-server-server/src/main/java/guyu/test/config/EnableEureka.java
@@ -0,0 +1,10 @@
+package guyu.test.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Component;
+
+@EnableEurekaServer
+@Component
+public class EnableEureka {
+
+}
